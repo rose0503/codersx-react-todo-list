56eb481b15f5101dc97a4785758f0135e238d283
Quoc <tqviet.0503@gmail.com>
2020-05-23 22:51:39 +0700

    fix feature todolist

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const todos = ["Go to market", "Buy food", "Make dinner"];
+// const items = []
+
+// for (let a of todos ){
+//   items.push(<li>{a}</li>)
+// }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          App ToDoList
+        </p>      
+        <ul>
+          {/* {items} */}
+          {todos.map((value) => {
+            return <li>{value}</li>
+          })}
+        </ul>         
+          
       </header>
     </div>
   );
